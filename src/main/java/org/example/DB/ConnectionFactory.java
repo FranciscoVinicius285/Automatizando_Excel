6cb3a24fe7816fa991b6f6c64afd501dbd3b979f
Vinicius <franciscovdsf2006@gmail.com>
2026-04-13 07:27:04 -0300

    Connection BD

---
+++ src/main/java/org/example/DB/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package org.example.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/ProjectEstagio";
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "barrocosql";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
